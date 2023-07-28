c72156522a4df5c8ebfa1202beb7c4359fb9830a
n.dzeranhouskaya <nadzeya.dzeranhouskaya@bglgroup.co.uk>
2023-07-28 14:17:14 +0300

    add courses block

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import { FC } from 'react';
 import Menu from './components/menu/index';
 import Title from './components/title/index';
 import Logos from './components/logos/index';
+import Courses from './components/courses/index';
 import './App.scss';
 
 const App: FC = () => {
   return (
     <>
-      <div className="firstScreen">
-        <Menu />
-        <Title />
-        <Logos />
-      </div>
-      courses
+      <Menu />
+      <Title />
+      <Logos />
+      <Courses />
     </>
   );
 };
